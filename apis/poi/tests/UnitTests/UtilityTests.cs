d8175c74e179504759bb99d28801a5ed72387d4c
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2020-09-02 14:53:01 -0500

    Fixing unit test

--- apis/poi/tests/UnitTests/UtilityTests.cs
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -11,7 +11,7 @@ namespace UnitTests
             Assert.Equal(1000, LoggingEvents.Healthcheck);
             Assert.Equal(2001, LoggingEvents.GetAllPOIs);
             Assert.Equal(2002, LoggingEvents.GetPOIByID);
-            Assert.Equal(2002777, LoggingEvents.GetPOIByTripID);
+            Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
         }
     }
 }
